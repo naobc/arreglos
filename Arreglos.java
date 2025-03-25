4937276fd1afe1bebf5899d094664441429f733f
Nao <naomibec123@gmail.com>
2025-03-25 14:03:47 -0600

    Adding 2 methods

--- Arreglos.java
+++ Arreglos.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Arreglos{
 
+    public static void leerCalificaciones(){
+
+    }
+
+    public static void evaluarCalificaciones({
+        
+    })
+
     public static void main(String[] args) {
         double[] calificaciones = new double[5];
         Scanner sc = new Scanner(System.in);
